fa847961574c83e73b3c9b1d160b6fea3ad5eec4
jpoulin587 <jpoulin587a@gmail.com>
2021-08-03 12:30:49 -0500

    get routes working

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api);
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
+// GET Route for notes page
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Route for error
 app.get('*', (req,res) => res.sendFile(path.join(__dirname, './public/404.html')));
 
 app.listen(PORT, () =>
