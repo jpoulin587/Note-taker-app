bbabefb2165e08ad2001cffc714a258dfe0eb0c6
jpoulin587 <jpoulin587a@gmail.com>
2021-08-03 13:15:56 -0500

    remove 404 page, fixed routes

--- script.js
+++ script.js
@@ -7,24 +7,20 @@ const PORT = process.env.port || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-//app.use(express.static('public'));
-
-// GET Route for homepage
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Route for error
-app.get('*', (req,res) => res.sendFile(path.join(__dirname, './public/404.html')));
+// Route for Index
+app.get('*', (req,res) => 
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
